a1538f0538e2849f247aab563996129d847b0a1d
farshad <farsmd@gmail.com>
2018-01-08 13:59:23 +0330

    Update db.php

--- afzir/css/db.php
+++ afzir/css/db.php
@@ -1,1 +1,1 @@
-<link rel='stylesheet' id='Afzir-css'  href='https://farsmd.github.io/afzir/css/afzir961018.css' type='text/css' media='all' />
+<link rel='stylesheet' id='Afzir-css'  href='https://farsmd.github.io/afzir/css/afzir961018.css' type='text/css' media='all' /> 
